2ccd7b36183008bc38e0ce0dd321dbcf2239a3e4
lostcrowgames <robincormie@protonmail.com>
2023-05-02 19:50:04 -0700

    Created Essential-Unity-Scripts folder to put all script assets in including scenes and materials. Updated Look At Target script with Gizmo colour.

--- Assets/Scripts/LookAtTarget.cs
+++ Assets/Scripts/LookAtTarget.cs
@@ -7,6 +7,8 @@ public class LookAtTarget : MonoBehaviour
     [SerializeField] private string targetTag;
     [SerializeField] private float detectionRange = 10f; // The range at which the Game Object can detect the target
     [SerializeField] private float rotationSpeed = 5f; // The speed at which the Game Object rotates to face the target
+    [SerializeField] private bool drawRangeGizmo;
+    [SerializeField] private Color gizmoColor = Color.yellow;
 
     private Transform lookAtTarget; // The transform of the target
 
@@ -33,15 +35,17 @@ public class LookAtTarget : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        // Draw a yellow wire circle to show the detection range
-        Gizmos.color = Color.yellow;
-        Vector3 circlePos = transform.position;
-        circlePos.y += 0.1f; // adjust the height to avoid z-fighting
-        float radius = detectionRange * Mathf.Max(transform.localScale.x, transform.localScale.z);
-        Matrix4x4 matrixBackup = Gizmos.matrix;
-        Gizmos.matrix = Matrix4x4.TRS(circlePos, Quaternion.LookRotation(Vector3.up, Vector3.forward), new Vector3(radius, radius, 0f));
-        Gizmos.DrawWireSphere(Vector3.zero, 1f);
-        Gizmos.matrix = matrixBackup;
+        if (drawRangeGizmo)
+        {
+            // Draw a yellow wire circle to show the detection range
+            Gizmos.color = gizmoColor;
+            Vector3 circlePos = transform.position;
+            circlePos.y += 0.1f; // adjust the height to avoid z-fighting
+            float radius = detectionRange * Mathf.Max(transform.localScale.x, transform.localScale.z);
+            Matrix4x4 matrixBackup = Gizmos.matrix;
+            Gizmos.matrix = Matrix4x4.TRS(circlePos, Quaternion.LookRotation(Vector3.up, Vector3.forward), new Vector3(radius, radius, 0f));
+            Gizmos.DrawWireSphere(Vector3.zero, 1f);
+            Gizmos.matrix = matrixBackup;
+        }
     }
-
 }
